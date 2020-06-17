fe255af287ee99879763d4cc6c5a596a22920352
Toly <scsiiq12345@gmail.com>
2020-06-17 08:34:45 +0300

    создавал коммиты через терминал редактора VScode

--- project/mai.js
+++ project/mai.js
@@ -1,6 +1,26 @@
+//создаем переменные, которые будут получать данные от пользователя
+var money = "Ваш бюджет на месяц?";
+var time = "Введите дату в формате YYYY-MM-DD";
 
-var answer = prompt("How you many?");
-var name = prompt("What's name you shop?");
+//создаем объект, который будет содержать такие данные
+var appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+}
 
-console.log(typeof(answer));
-console.log(typeof(name));
+//выводим на экран вопросы для ввода ответа, и в конце считает рассчет бюджета
+appData.budget = prompt(money);
+appData.timeData = prompt(time);
+appData.optionalExpenses.article = prompt('Введите обязательную статью расходов в этом месяце');
+appData.optionalExpenses.howMany = prompt('Во сколько обойдется?');
+
+alert("Ваш бюджет на 1 день равняется " + appData.budget / 30);
+
+console.log(appData.budget);
+console.log(appData.timeData);
+console.log(appData.optionalExpenses.article);
+console.log(appData.optionalExpenses.howMany);
